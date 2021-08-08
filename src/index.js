a40f12023b6b67334f9ef43e75bedd5d1945abe8
Oleh-Li <Oleh.7ee@gmail.com>
2021-08-08 20:09:24 +0300

    add submit on search img form

--- src/index.js
+++ src/index.js
@@ -1,48 +1,41 @@
 import './sass/main.scss';
 import './sass/photocard.scss'
-import {
-  debounce
-} from "lodash"
 import template from "./template.hbs"
-import callAlert from "./pnotifyCode.js"
 import InfiniteScroll from "infinite-scroll"
 
+const form = document.querySelector("#search-form")
 const input = document.querySelector("input")
 const list = document.querySelector(".gallery")
-const baseUrl = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=car&page=1&per_page=3&key=15354044-5c6c2e030b5f90cfcf13d54e3'
 
-const feedContainer = document.querySelector('#feed');
-const infScrollInstance = new InfiniteScroll(feedContainer, {
+
+let inputValue = ''
+const infScrollInstance = new InfiniteScroll(list, {
   path() {
-    return`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=car&page=${this.pageIndex}&per_page=3&key=15354044-5c6c2e030b5f90cfcf13d54e3`
+    return `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${inputValue}&page=${this.pageIndex}&per_page=6&key=15354044-5c6c2e030b5f90cfcf13d54e3`
   },
   responseBody: 'json',
   status: '.scroll-status',
   history: false,
+})
+
+infScrollInstance.on("load", response => {
+  const images = response.hits
+  list.insertAdjacentHTML("beforeend", template(images))
 });
 
-infScrollInstance.on("load", response=>{
-  const posts = response.hits
-  feedContainer.insertAdjacentHTML("beforeend", template(posts))
-});
+function handleInput(e) {
+  e.preventDefault()
+  inputValue = input.value.trim()
+  if (inputValue) {
+    list.innerHTML = "";
+    infScrollInstance.pageIndex = 1
+    infScrollInstance.option({
+      path() {
+        return `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${inputValue}&page=${this.pageIndex}&per_page=6&key=15354044-5c6c2e030b5f90cfcf13d54e3`
+      }
+    })
+    infScrollInstance.loadNextPage()
+  }
+}
 
-infScrollInstance.loadNextPage()
-
-
-/////////do before
-// fetch(baseUrl)
-//   .then(response => {
-//     return response.json()
-//   })
-//   .then(data => list.insertAdjacentHTML("beforeend", template(data.hits)))
-
-// function handleInput(e) {
-//   const inputValue = e.target.value
-//   if (!inputValue.trim()) {
-//     return
-//   }
-//   console.log(inputValue.trim())
-//   return inputValue.trim()
-// }
-
-// input.addEventListener("input", debounce(handleInput, 500))
+form.addEventListener("submit", handleInput)
